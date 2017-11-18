1879bc267e53831cea463a2cdb5799f5f675cede
eagleweb <eagleweb@mail.ru>
2017-11-18 19:47:42 +0200

    Add

--- js/js.js
+++ js/js.js
@@ -1,5 +1,17 @@
-function RemoveElementFromShop() {
+function RemoveLastElementFromShop() {
     var elem = document.getElementById('shop').lastElementChild;
-    console.log(elem);
     elem.remove();
+}
+
+function AddElementToShop() {
+    var parentElem = document.getElementById('shop');
+    var item = document.getElementById('item-input-form').cloneNode(true);
+    var fragment = document.createDocumentFragment();
+    fragment.appendChild(item);
+
+    var div = document.createElement('div');
+    div.className = "order_form";
+    div.appendChild(fragment);
+
+    parentElem.appendChild(div);
 }
